610942ccef079a3cd52d202848201895154fb462
M Subhan Khan <126487035+MSubhanKhan786000@users.noreply.github.com>
2024-09-08 19:41:46 +0000

    Pagination Completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,71 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getProducts } from "./services/productService";
 import "./styles.css";
 
-export default function App() {
+const App = () => {
   const [products, setProducts] = useState([]);
-  console.log(products);
+  const [pages, setPages] = useState(1);
+  console.log(pages);
+
+  const handlePagination = (selectedPage) => {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= totalPages &&
+      selectedPage != pages
+    ) {
+      setPages(selectedPage);
+    }
+  };
+
   useEffect(() => {
     getProducts()
       .then((res) => setProducts(res.data.products))
       .catch((err) => console.warn(err));
   }, []);
+
+  const productsPerPage = 10;
+  const totalPages = Math.ceil(products.length / productsPerPage);
+
+  const handlePrev = () => {
+    handlePagination(pages - 1);
+  };
+
+  const handleNext = () => {
+    handlePagination(pages + 1);
+  };
+
   return (
-    <div className="App">
-      <h1>Pagination Application</h1>
-      {/* Get  products on frontend*/}
-      {products.map((product) => (
-        <p key={product.id}>{product.title}</p> // Display each product's title
-      ))}
+    <div className="container">
+      <div className="product-list">
+        {products
+          .slice((pages - 1) * productsPerPage, pages * productsPerPage)
+          .map((product) => (
+            <div className="product-item" key={product.id}>
+              <img
+                src={product.thumbnail}
+                alt={product.title}
+                height={200}
+                width={200}
+              />
+              <p>{product.title}</p>
+            </div>
+          ))}
+      </div>
+      <div className="pagination">
+        <span onClick={handlePrev}>◀️</span>
+        {Array.from({ length: totalPages }, (_, i) => (
+          <span
+            onClick={() => handlePagination(i + 1)}
+            className="page__no"
+            key={i}
+          >
+            {i + 1}
+          </span>
+        ))}
+        <span onClick={handleNext}>▶️</span>
+      </div>
     </div>
   );
-}
+};
+
+export default App;
